6730f8d1807348f7a242d8f1d1d26f64067db6ac
Rednz B.A.990 <red.id9990@gmail.com>
2026-07-03 07:55:08 +0800

    Fixing the sh*t JS. And mewing 1000x Times to get fixed
    Refactor getDynamicFilteredItems function to simplify item filtering logic and remove unused variables.

--- scripts/global-dynamicFilteredItems.js
+++ scripts/global-dynamicFilteredItems.js
@@ -3,26 +3,27 @@ global.getDynamicFilteredItems = function() {
     const allItems = Vars.content.items();
 
     const isSandbox = Vars.state.rules.infiniteResources;
-    const currentSector = Vars.state.rules.sector;
-    const currentPlanet = currentSector != null ? currentSector.planet : null;
+    // const currentSector = Vars.state.rules.sector;
+    // const currentPlanet = currentSector != null ? currentSector.planet : null;
 
     for (let i = 0; i < allItems.size; i++) {
         let item = allItems.get(i);
 
+        if (item == undefined || item == null) continue;
         if (item.name.endsWith("-liquid-item")) continue;
         if (item.isHidden()) continue;
 
-        let isAllowedOnPlanet = true;
-        if (!isSandbox) {
-            if (currentPlanet != null && currentPlanet.hiddenItems.contains(item)) {
-                isAllowedOnPlanet = false;
-            }
-            if (Vars.state.rules.hiddenBuildItems.contains(item)) {
-                isAllowedOnPlanet = false; 
-            }
-        }
+        // let isAllowedOnPlanet = true;
+        // if (!isSandbox) {
+           //  if (currentPlanet != null && currentPlanet.hiddenItems.contains(item)) {
+              //   isAllowedOnPlanet = false;
+        //     }
+         //    if (Vars.state.rules.hiddenBuildItems.contains(item)) {
+         //        isAllowedOnPlanet = false; 
+         //    }
+        // }
 
-        if (isSandbox || (isAllowedOnPlanet && item.unlockedNow())) {
+        if (isSandbox || item.unlockedNow()) {
             allowedItems.add(item);
         }
     }
